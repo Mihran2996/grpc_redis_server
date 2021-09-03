fdfbfffac017e7705f32fba6e41c3bc10cbcd932
Mihran Maruqyan <mihran_maruqyan@epam.com>
2021-09-03 11:07:49 +0400

    Initial commit

---
+++ src/test/java/com/example/grpc_redis_server/GrpcRedisServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpc_redis_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcRedisServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
